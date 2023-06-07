0c622909ff22b9598fa5217ef7ab31d1d5255148
Konrad <konslo96@gmail.com>
2023-06-07 17:11:43 +0200

    style: Change repertoire layout

--- src/components/Repertoire/Repertoire.tsx
+++ src/components/Repertoire/Repertoire.tsx
@@ -19,24 +19,16 @@ const Repertoire = () => {
 
   return (
     <section id="repertoire" className={classes.root}>
-      <div className={classes.head_container}>
-        <h3 className={classes.heading}>REPERTUAR</h3>
-        <div className={classes.image}>
-          <Image
-            src={singleRepertoire.image}
-            alt="repertoire5"
-            priority={true}
-          />
-        </div>
+      <h3 className={classes.heading}>REPERTUAR</h3>
+      <div className={classes.image}>
+        <Image src={singleRepertoire.image} alt="repertoire5" priority={true} />
       </div>
-      <div className={classes.container}>
-        <div className={classes.sub_heading}>
-          <h4>{singleRepertoire.title}</h4>
-          <h5>{singleRepertoire.subtitle}</h5>
-        </div>
-        <div className={classes.content}>
-          <p>{singleRepertoire.description}</p>
-        </div>
+      <div className={classes.sub_heading}>
+        <h4>{singleRepertoire.title}</h4>
+        <h5>{singleRepertoire.subtitle}</h5>
+      </div>
+      <div className={classes.content}>
+        <p>{singleRepertoire.description}</p>
         <div className={classes.switcher}>
           {REPERTOIRE.map((item) => {
             return (
